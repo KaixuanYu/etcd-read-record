f1854643f8639b18c5ad8a72819f6fa82fd52e53
yukaixuan <2358685567@qq.com>
2020-10-13 12:45:55 +0800

    wal util.go 阅读

--- wal/util.go
+++ wal/util.go
@@ -27,6 +27,7 @@ import (
 var errBadWALName = errors.New("bad wal name")
 
 // Exist returns true if there are any files in a given directory.
+// 判断给定的文件夹中是否有 .wal文件，etcd默认的看 default.etcd/member/wal 下是否有.wal为扩展名的字段。
 func Exist(dir string) bool {
 	names, err := fileutil.ReadDir(dir, fileutil.WithExt(".wal"))
 	if err != nil {
@@ -38,6 +39,9 @@ func Exist(dir string) bool {
 // searchIndex returns the last array index of names whose raft index section is
 // equal to or smaller than the given index.
 // The given names MUST be sorted.
+//searchIndex 返回 raft index section 等于或小于给定索引的names参数的的最后一个数组索引。
+//参数 names 必须是有序的
+// 就是比如有0-1.wal 0-2.wal 0-3.wal 0-4.wal ，传入的index是3,那么就会返回index=2。
 func searchIndex(lg *zap.Logger, names []string, index uint64) (int, bool) {
 	for i := len(names) - 1; i >= 0; i-- {
 		name := names[i]
@@ -53,7 +57,9 @@ func searchIndex(lg *zap.Logger, names []string, index uint64) (int, bool) {
 }
 
 // names should have been sorted based on sequence number.
+// 参数names应该根据序号排序。
 // isValidSeq checks whether seq increases continuously.
+// isValidSeq检查seq是否递增。
 func isValidSeq(lg *zap.Logger, names []string) bool {
 	var lastSeq uint64
 	for _, name := range names {
@@ -81,6 +87,7 @@ func readWALNames(lg *zap.Logger, dirpath string) ([]string, error) {
 	return wnames, nil
 }
 
+//检查给定的names参数是否是 xxxx-xxxxx.wal 格式的。返回符合格式的names，是不是叫filterNotWalNames更好。
 func checkWalNames(lg *zap.Logger, names []string) []string {
 	wnames := make([]string, 0)
 	for _, name := range names {
@@ -99,6 +106,7 @@ func checkWalNames(lg *zap.Logger, names []string) []string {
 	return wnames
 }
 
+//解析wal名称，返回xxx1-xxx2.wal中的xxx1和xxx2,如果不符合这种格式返回err
 func parseWALName(str string) (seq, index uint64, err error) {
 	if !strings.HasSuffix(str, ".wal") {
 		return 0, 0, errBadWALName
@@ -107,6 +115,7 @@ func parseWALName(str string) (seq, index uint64, err error) {
 	return seq, index, err
 }
 
+//生成wal name
 func walName(seq, index uint64) string {
 	return fmt.Sprintf("%016x-%016x.wal", seq, index)
 }
