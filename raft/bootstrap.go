c115498a339f33439c1d7a176e6b385753c243fe
yukaixuan <yukaixuan@100tal.com>
2020-10-19 18:39:21 +0800

    raft bootstrap

--- raft/bootstrap.go
+++ raft/bootstrap.go
@@ -23,10 +23,12 @@ import (
 // Bootstrap initializes the RawNode for first use by appending configuration
 // changes for the supplied peers. This method returns an error if the Storage
 // is nonempty.
+// Bootstrap初始化RawNode以供首次使用，方法是为所提供的对等节点附加配置更改。如果存储空间不空，此方法将返回错误。
 //
 // It is recommended that instead of calling this method, applications bootstrap
 // their state manually by setting up a Storage that has a first index > 1 and
 // which stores the desired ConfState as its InitialState.
+//建议应用程序不要调用此方法，而是通过设置一个第一个索引大于1并将所需的ConfState存储为其InitialState的存储器来手动引导它们的状态。
 func (rn *RawNode) Bootstrap(peers []Peer) error {
 	if len(peers) == 0 {
 		return errors.New("must provide at least one peer to Bootstrap")
@@ -43,10 +45,12 @@ func (rn *RawNode) Bootstrap(peers []Peer) error {
 	// We've faked out initial entries above, but nothing has been
 	// persisted. Start with an empty HardState (thus the first Ready will
 	// emit a HardState update for the app to persist).
+	//我们伪造了上面的初始条目，但没有任何内容被持久化。从一个空的HardState开始（因此第一个Ready将发出一个HardState更新，以便应用程序保持不变）。
 	rn.prevHardSt = emptyState
 
 	// TODO(tbg): remove StartNode and give the application the right tools to
 	// bootstrap the initial membership in a cleaner way.
+	//TODO（tbg）：删除StartNode并为应用程序提供正确的工具，以更干净的方式引导初始成员身份。
 	rn.raft.becomeFollower(1, None)
 	ents := make([]pb.Entry, len(peers))
 	for i, peer := range peers {
@@ -69,6 +73,10 @@ func (rn *RawNode) Bootstrap(peers []Peer) error {
 	// entries since they have already been committed).
 	// We do not set raftLog.applied so the application will be able
 	// to observe all conf changes via Ready.CommittedEntries.
+	//现在应用它们，主要是为了使应用程序可以在测试中的StartNode之后立即调用Campaign。
+	// 请注意，这些节点将两次添加到筏中：在此处以及应用程序的Ready循环调用ApplyConfChange时。
+	// 对addNode的调用必须在所有对raftLog.append的调用之后进行，因此progress.next是在这些引导条目之后设置的（如果由于已经提交而试图附加这些条目，则是错误的）。
+	// 我们不设置raftLog.applied 因此应用程序将能够通过Ready.CommittedEntries观察所有配置更改
 	//
 	// TODO(bdarnell): These entries are still unstable; do we need to preserve
 	// the invariant that committed < unstable?
